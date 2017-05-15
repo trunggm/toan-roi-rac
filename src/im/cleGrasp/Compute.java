ab17445a2b8fbbfc6c4127be03a0ef4b796050fa
kid-dorr <kiddorr430@gmail.com>
2017-05-15 10:01:16 +0700

    add runtime

--- src/im/cleGrasp/Compute.java
+++ src/im/cleGrasp/Compute.java
@@ -131,7 +131,14 @@ public class Compute {
 			if(best.cost > cadidate.cost){
 				best = cadidate;
 			}
+			if (i % 1000 == 0) {
+				double percent = round((double) i /max_iter * 100, 1);
+				String progress = "\rProgress " + Double.toString(percent) + "%:\tCurrent best value:" + best.cost;
+				System.out.print(progress);
+			}
 		}
+		
+		
 		
 		return best;
 	}
@@ -141,4 +148,77 @@ public class Compute {
 		return null;
 	}
 	
+	public static double[][] berlin52 = {{565,575},{25,185},{345,750},{945,685},{845,655},
+            {880,660},{25,230},{525,1000},{580,1175},{650,1130},{1605,620},
+            {1220,580},{1465,200},{1530,5},{845,680},{725,370},{145,665},
+            {415,635},{510,875},{560,365},{300,465},{520,585},{480,415},
+            {835,625},{975,580},{1215,245},{1320,315},{1250,400},{660,180},
+            {410,250},{420,555},{575,665},{1150,1160},{700,580},{685,595},
+            {685,610},{770,610},{795,645},{720,635},{760,650},{475,960},
+            {95,260},{875,920},{700,500},{555,815},{830,485},{1170,65},
+            {830,610},{605,625},{595,360},{1340,725},{1740,245}};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int a = 5;
+//		int b = a << 2;
+//		int[] c = {1, 2};
+//		int[] d = c;
+//		Random rand = new Random();
+//		int  n = rand.nextInt(50) + 1;
+//		//System.out.println(round(200.23123, 0));
+//		
+//		int[][] f = {{1, 2}, {1, 2}};
+//		int[][] f1 = f;
+//		System.out.println(f1[1][1]);
+		
+		
+		double[][] cities = berlin52;
+		
+		// Display data
+		System.out.println("/***************************************************************/");
+		System.out.println("The Greedy Randomized Adaptive Search - GRASP");
+		System.out.println("The Berlin52 instance for GRASP");
+		System.out.println("The optimal tour distance for Berlin52 instance is 7542 units");
+		System.out.println("Author: Trunggm");
+		System.out.println("/***************************************************************/");
+		System.out.println("Data:");
+		System.out.println("* Number of cities: " + cities.length);
+		System.out.println("* List coordinates cities: ");
+		for (int i = 0; i < cities.length; i+=4) {
+			System.out.println(i + "/ [" + cities[i][0] + ", " + cities[i][1] + "]\t"
+								+ (i+1) + "/ [" + cities[i+1][0] + ", " + cities[i+1][1] + "]\t"
+								+ (i+2) + "/ [" + cities[i+2][0] + ", " + cities[i+2][1] + "]\t"
+								+ (i+3) + "/ [" + cities[i+3][0] + ", " + cities[i+3][1] + "]");
+		}
+		
+		
+		int max_iter = 100000;
+		int max_no_improve = 50;
+		float greediness_factor = (float) 0.3;
+		
+		System.out.println("/***************************************************************/");
+		System.out.println("Compute:");
+		System.out.println("* Max iteration: \t\t" + max_iter);
+		System.out.println("* Max number of improve: \t" + max_no_improve);
+		System.out.println("* Greediness coefficient: \t" +  greediness_factor);
+		
+		Compute com = new Compute();
+		
+		Candidate best = com.search(cities, max_iter, max_no_improve, greediness_factor);
+		System.out.println("");
+		System.out.println("/***************************************************************/");
+		String result = "";
+		
+		for(int i=0; i<best.vector.size();i++){
+			result += best.vector.get(i) + ((i<best.vector.size()-1) ? "->" : "");
+		}
+		
+		
+		System.out.println("Final:");
+		System.out.println("* Solution computed: " + best.cost + " units.");
+		System.out.println("* Final tour path: ");
+		System.out.println(result);
+		
+	}
 }
